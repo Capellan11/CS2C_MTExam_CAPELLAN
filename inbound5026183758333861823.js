63c4ebdc8e9b329c56b205f0e0e607b3af66d51f
Capellan11 <aureocapellan11@gmail.com>
2024-10-30 20:32:45 +0800

    Add files via upload

---
+++ inbound5026183758333861823.js
@@ -0,0 +1,9 @@
+let fullName = "Aureo Leaño Capellan jr.";
+let age = 19;
+let favoriteNumber = 46;
+let favoriteColor = "red";
+
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
